786cacac4c88a1f36ded83bd29137df89365fb20
Thorhallur Helgason <laddi@idega.com>
2007-02-07 20:46:31 +0000

    Updated version of block.

--- src/java/com/idega/block/calendar/data/CalendarCategoryBMPBean.java
+++ src/java/com/idega/block/calendar/data/CalendarCategoryBMPBean.java
@@ -16,6 +16,7 @@ public class CalendarCategoryBMPBean extends com.idega.block.category.data.ICCat
   public CalendarCategoryBMPBean(){
     super();
   }
+  
   public CalendarCategoryBMPBean(int id)throws SQLException{
     super(id);
   }
